93e35388c532e90df00c864c483a388c4957ec41
BCsabaA <barabas.csabi@gmail.com>
2025-01-06 07:57:53 +0100

    import and settings view, init_db updated with user settings

--- simplecards/schema_update.sql
+++ simplecards/schema_update.sql
@@ -1,8 +1,34 @@
-CREATE TABLE user_selections (
+CREATE TABLE learn_mode (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(30)
+);
+
+INSERT INTO learn_mode (name) VALUES
+    ("by name A to Z"),
+    ("by name Z to A"),
+    ("by id 1 to n"),
+    ("by id n to 1"),
+    ("random");
+
+CREATE TABLE read_time (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(30)
+);
+
+INSERT INTO read_time (name) VALUES
+    ("by length of text"),
+    ("set manually");
+
+CREATE TABLE user_settings (
     user_id INTEGER PRIMARY KEY,
-    selected_group_id INTEGER DEFAULT NULL,
-    selected_deck_id INTEGER DEFAULT NULL,
-    FOREIGN KEY (user_id) REFERENCES user (id),
-    FOREIGN KEY (selected_group_id) REFERENCES groups (id),
-    FOREIGN KEY (selected_deck_id) REFERENCES deck (id)
+    learn_mode_id INTEGER DEFAULT 1,
+    repeat_list INTEGER DEFAULT 0,
+    read_time_id INTEGER DEFAULT 1,
+    ms_per_char INTEGER DEFAULT 50,
+    q_min_read INTEGER DEFAULT 1000,
+    a_min_read INTEGER DEFAULT 3000,
+    q_read INTEGER DEFAULT 2000,
+    a_read INTEGER DEFAULT 5000,
+    FOREIGN KEY (learn_mode_id) REFERENCES learn_mode (id),
+    FOREIGN KEY (read_time_id) REFERENCES read_time (id)
 );
